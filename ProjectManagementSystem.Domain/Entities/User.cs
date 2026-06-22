36b2b75f2e9944093508439c2d513d0de8978e1d
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 16:16:59 +0530

    Add User Entity to Domain Layer

---
+++ ProjectManagementSystem.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProjectManagementSystem.Domain.Common;
+using ProjectManagementSystem.Domain.Enums;
+
+namespace ProjectManagementSystem.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; }
+}
