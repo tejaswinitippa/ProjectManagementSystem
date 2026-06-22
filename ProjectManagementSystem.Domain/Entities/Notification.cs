342b07903d0837c338ce529fa6150fcf69113c03
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 16:38:12 +0530

    Add Notification Entity to Domain Layer

---
+++ ProjectManagementSystem.Domain/Entities/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProjectManagementSystem.Domain.Common;
+
+namespace ProjectManagementSystem.Domain.Entities;
+
+public class Notification : BaseEntity
+{
+    public string Message { get; set; } = string.Empty;
+
+    public bool IsRead { get; set; }
+
+    public Guid UserId { get; set; }
+}
