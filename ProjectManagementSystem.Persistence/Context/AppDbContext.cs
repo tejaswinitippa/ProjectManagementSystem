6502c4529624f924ada1c23325faf8beff50a9ef
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 16:59:19 +0530

    Setup Entity Framework Core and AppDbContext

---
+++ ProjectManagementSystem.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Domain.Entities;
+
+namespace ProjectManagementSystem.Persistence.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Project> Projects { get; set; }
+
+    public DbSet<TaskItem> Tasks { get; set; }
+
+    public DbSet<Notification> Notifications { get; set; }
+}
