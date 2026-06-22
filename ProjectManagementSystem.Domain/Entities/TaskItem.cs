e8892364c0e15a91d8db1a9a2d684900fa39e472
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 16:37:08 +0530

    Add TaskItem Entity to Domain Layer

---
+++ ProjectManagementSystem.Domain/Entities/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectManagementSystem.Domain.Common;
+using ProjectManagementSystem.Domain.Enums;
+using TaskStatus = ProjectManagementSystem.Domain.Enums.TaskStatus;
+
+namespace ProjectManagementSystem.Domain.Entities;
+
+public class TaskItem : BaseEntity
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public TaskPriority Priority { get; set; }
+
+    public TaskStatus Status { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public Guid AssignedUserId { get; set; }
+
+    public Guid ProjectId { get; set; }
+}
