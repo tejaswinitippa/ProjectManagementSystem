37dbcfd422457bf29e6c8be4c08112b9eb33263d
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 16:37:01 +0530

    Add Project Entity to Domain Layer

---
+++ ProjectManagementSystem.Domain/Entities/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectManagementSystem.Domain.Common;
+
+namespace ProjectManagementSystem.Domain.Entities;
+
+public class Project : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+}
