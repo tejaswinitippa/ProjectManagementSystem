d90ba5d5b3fb5c053160d1685b83a896241e42ae
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 19:42:48 +0530

    Implement user repository

---
+++ ProjectManagementSystem.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Application.Interfaces.Repositories;
+using ProjectManagementSystem.Domain.Entities;
+using ProjectManagementSystem.Persistence.Context;
+
+namespace ProjectManagementSystem.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(x => x.Email == email);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
