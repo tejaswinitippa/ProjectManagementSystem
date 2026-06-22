53c1937c2a17ffaeb49f92848e95021e9df7229c
Tejaswinitippa <tippatejaswini18@gmail.com>
2026-06-22 16:08:20 +0530

    Add Domain Enums for Roles, Status and Priority

---
+++ ProjectManagementSystem.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ProjectManagementSystem.Domain.Enums;
+
+public enum TaskStatus
+{
+    Todo = 1,
+    InProgress = 2,
+    Testing = 3,
+    Done = 4
+}
---
+++ ProjectManagementSystem.Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.Domain.Enums;
+
+public enum UserRole
+{
+    Admin = 1,
+    Manager = 2,
+    Developer = 3
+}
